48adb0dba79f699e545b0c16053a613f43e2e94a
Victor Delgado <vdel26@gmail.com>
2014-08-24 21:30:18 +0200

    feat(landing): login&register point to app.medistream.es

--- Gruntfile.js
+++ Gruntfile.js
@@ -279,6 +279,7 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
+            'CNAME',
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
